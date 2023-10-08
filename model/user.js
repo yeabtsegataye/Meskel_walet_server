1824d42ca740665a6ba16bd8041b4fd10d5b38ec
Betelhem-Belete <betelhembelete0@gmail.com>
2023-10-08 18:41:55 -0400

    deploy

--- model/user.js
+++ model/user.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const schema = mongoose.Schema;
 
 const userModel = schema(
   {
-    Email: { type: "String", required: true, unique: true },
-    password: { type: "String", required: true },
+    Email: { type: 'String', required: true, unique: true },
+    password: { type: 'String', required: true },
     isAdmin: {
       type: Boolean,
       required: true,
@@ -15,20 +15,15 @@ const userModel = schema(
   { timestamps: true }
 );
 
-userModel.statics.signup = async function (
-  Email,
-  password,
-  isAdmin,
-) {
+userModel.statics.signup = async function (Email, password, isAdmin) {
   if (!password || !Email) {
-    throw Error("fill the space first");
+    throw Error('fill the space first');
   }
   const match = await this.findOne({ Email });
 
   if (match) {
-    throw Error("user alrady exists");
+    throw Error('user alrady exists');
   }
- 
 
   const hashed = await bcrypt.hash(password, 10);
   const user = await this.create({
@@ -40,20 +35,20 @@ userModel.statics.signup = async function (
 };
 userModel.statics.login = async function (Email, password) {
   if (!Email || !password) {
-    throw Error("All fields must be filled");
+    throw Error('All fields must be filled');
   }
 
   const user = await this.findOne({ Email });
   if (!user) {
-    throw Error("user not found");
+    throw Error('user not found');
   }
 
   const match = await bcrypt.compare(password, user.password);
   if (!match) {
-    throw Error("Incorrect password");
+    throw Error('Incorrect password');
   }
 
   return user;
 };
-const User = mongoose.model("user", userModel);
+const User = mongoose.model('user', userModel);
 module.exports = User;
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require('dotenv')
-dotenv.config()
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 //////
-const cors = require("cors");
-const routers = require("./router/meskel");
-const user = require('./router/user')
+const cors = require('cors');
+const routers = require('./router/meskel');
+const user = require('./router/user');
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect(process.env.MONGO_DB).then(() => {
   app.listen(process.env.PORT, () => {
-    console.log("listening on port :", process.env.PORT);
+    console.log('listening on port :', process.env.PORT);
   });
 });
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
-app.use(user)
+app.use(user);
 app.use(routers);
