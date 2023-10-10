448619b6c6dfba87e5ccd327a3b0bb327f2b527c
tati <tatitaye0@gmail.com>
2023-10-10 18:29:00 +0300

    final touch

--- router/meskel.js
+++ router/meskel.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const {handle_verify} = require('../middleware/Aut_middleware')
 const Router = express.Router()
 const { handle_get_all, handle_add, handle_delete, handle_edit, handle_get_one } = require('../controller/cash_controller')
 
 Router.get('/', handle_get_all)
 Router.get('/:id', handle_get_one)
+// Router.use(handle_verify)
 Router.post('/add', handle_add)
 Router.put('/edit/:id', handle_edit)
 Router.delete('/delete/:id', handle_delete)
--- router/user.js
+++ router/user.js
@@ -4,7 +4,8 @@ const {
   registerUser,
   authUser,
 } = require("../controller/user_cont");
-
+const {handle_verify} = require('../middleware/Aut_middleware')
+Router.post("/verify", handle_verify);
 Router.post("/login", authUser);
 Router.post("/signup", registerUser);
 
